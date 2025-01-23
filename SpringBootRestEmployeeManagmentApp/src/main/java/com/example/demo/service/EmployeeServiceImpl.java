d29b8fb3119f945464e1cb63055710c813b643ce
VenkyAA <vjonala@gmail.com>
2025-01-23 12:58:19 +0530

    Add request done

--- SpringBootRestEmployeeManagmentApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ SpringBootRestEmployeeManagmentApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -1,5 +1,26 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+	@Autowired
+	EmployeeRepository repository;
+	
+	@Override
+	public String addEmployee(Employee employee) {
+		Employee emp = repository.save(employee);
+		if(emp != null) {
+			return "Employee saved successfully";
+		}
+		else {
+			return "Failed to save Employee";
+		}
+	}
 
 }
